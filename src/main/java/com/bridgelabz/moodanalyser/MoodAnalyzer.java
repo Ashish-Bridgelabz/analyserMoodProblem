886563811725c09b749264f0e61f66966adbd20b
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-04 13:01:39 +0530

    ADDED:Displayed null mood return happy

---
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyzer.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.moodanalyser;
+
+public class MoodAnalyzer {
+    private String message;
+
+    //NO argument parameters constructor
+    MoodAnalyzer() {
+    }
+
+    //Parameters constructor
+    MoodAnalyzer(String message) {
+        this.message = message;
+    }
+
+    public String analyzeMood(String message) {
+        this.message = message;
+        return analyzeMood();
+    }
+
+    public String analyzeMood() {
+        try {
+            if (message.contains("Sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        } catch (NullPointerException e) {
+            return "HAPPY";
+        }
+    }
+}
+
+
