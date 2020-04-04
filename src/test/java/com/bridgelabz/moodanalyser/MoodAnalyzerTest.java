a3670d06b26c4557f10bf3ad1acc03b92ede25aa
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-04 13:03:24 +0530

    ADDED:Test case for input as null mood which return happy

---
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyzerTest.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.moodanalyser;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyzerTest {
+        @Test
+        public void givenMessage_WhenProper_ShouldReturnSadMood() {
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am a Sad Mood");
+            String result = moodAnalyzer.analyzeMood();
+            Assert.assertEquals("SAD", result);
+        }
+    @Test
+    public void givenMoodAnalyzer_WhenMessageNotIncludesSad_ShouldReturnHappy() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
+        String result = moodAnalyzer.analyzeMood();
+        Assert.assertEquals("HAPPY", result);
+    }
+}
