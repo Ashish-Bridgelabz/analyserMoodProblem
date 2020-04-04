96fa032e72c6e32cef95dd87efbfed971b1f71f1
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-04 21:32:00 +0530

    ADDED:checkd the exception

--- src/main/java/com/bridgelabz/moodanalyser/MoodAnalysisException.java
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalysisException.java
@@ -1,16 +1,17 @@
 package com.bridgelabz.moodanalyser;
 
 public class MoodAnalysisException extends Exception {
+
+
     enum ExceptionType {
-        ENTERED_NULL,ENTERED_EMPTY
+        ENTERED_NULL, ENTERED_EMPTY,
+        NO_SUCH_CLASS, NO_SUCH_METHOD
     }
 
     ExceptionType type;
 
-    public MoodAnalysisException(String message, ExceptionType type) {
+    public MoodAnalysisException(ExceptionType type, String message) {
         super(message);
         this.type = type;
     }
-
 }
-
