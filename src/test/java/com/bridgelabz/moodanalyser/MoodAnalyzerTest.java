a593c6f62434f8b53525f55aa97c53eb77a4f6b2
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-04 13:43:06 +0530

    ADDED:Test Case pass Handle NULL

--- src/test/java/com/bridgelabz/moodanalyser/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyzerTest.java
@@ -1,19 +1,35 @@
 package com.bridgelabz.moodanalyser;
 
+import javafx.scene.chart.ScatterChart;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyzerTest {
-        @Test
-        public void givenMessage_WhenProper_ShouldReturnSadMood() {
-            MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am a Sad Mood");
-            String result = moodAnalyzer.analyzeMood();
-            Assert.assertEquals("SAD", result);
-        }
+    @Test
+    public void givenMessage_WhenProper_ShouldReturnSadMood() throws MoodAnalysisException {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am a Sad Mood");
+        String result = moodAnalyzer.analyzeMood();
+        Assert.assertEquals("SAD", result);
+    }
+
     @Test
     public void givenMoodAnalyzer_WhenMessageNotIncludesSad_ShouldReturnHappy() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
-        String result = moodAnalyzer.analyzeMood();
-        Assert.assertEquals("HAPPY", result);
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in any mood");
+        String result = null;
+        try {
+            result = moodAnalyzer.analyzeMood();
+        } catch (MoodAnalysisException e) {
+            Assert.assertEquals("HAPPY", result);
+        }
+    }
+
+    @Test
+    public void givenMoodAnalyzer_WhenMessageNull_ShouldThrowsException() {
+        try {
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
+            String results = moodAnalyzer.analyzeMood();
+        } catch (MoodAnalysisException e) {
+            Assert.assertEquals("Please enter a valid mood", e.getMessage());
+        }
     }
 }
