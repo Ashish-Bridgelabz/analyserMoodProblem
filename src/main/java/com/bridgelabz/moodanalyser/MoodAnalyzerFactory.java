bc4e2f8a35e62d2779b9847cfe6c84f14503bc25
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-04 21:35:24 +0530

    ADDED:Created moodAnalyserFactory and static method

---
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyzerFactory.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.moodanalyser;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class MoodAnalyzerFactory {
+
+    public static MoodAnalyzer createMoodAanayzerObject() throws MoodAnalysisException {
+        try {
+            Class<?> moodAnalyzer = Class.forName("MoodAnalyzerMood");
+            Constructor<?> moodAnalysisConstructor = moodAnalyzer.getConstructor();
+            Object moodObject = moodAnalysisConstructor.newInstance();
+            return (MoodAnalyzer) moodObject;
+        } catch (ClassNotFoundException e) {
+            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NO_SUCH_CLASS, "Class not found ");
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
